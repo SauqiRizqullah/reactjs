2f516b01a7f61f6b8e0a9362db0dd5a91ad77b0b
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2025-02-12 10:27:48 +0700

    [ADD] Custom Hook useHomeFetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,38 +7,14 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMGAE_BASE_URL } from '../config'
 //Components
 
 //Hook
+import { useHomeFetch } from '../hooks/userHomeFetch';
 
 //Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
 
-    const [error, setError] = useState(false);
-    const fetchMovies = async (page, searchTerm = "") => {
-        try {
-            setError(false);
-            setLoading(true);
-
-            const movies= await API.fetchMovies(searchTerm, page);
-            // console.log(movies);
-
-            setState(prev => ({
-                ...movies, 
-                results:
-                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results] 
-        }));
-        } catch (error) {
-            setError(true);
-        }
-        setLoading(false);
-    }
-
-    // Initial Render
-    useEffect(() => {
-        fetchMovies(1)
-    }, [])
+    const {state, loading, error} = useHomeFetch();
 
     console.log(state);
 

5e72761c4fad6c805bb58849a38d55e007d3bc17
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2025-02-12 13:43:10 +0700

    [ADD] HeroImage Component with Styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from 'react'
 import API from '../API';
 
 //Config
-import { POSTER_SIZE, BACKDROP_SIZE, IMGAE_BASE_URL } from '../config'
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 //Components
+import HeroImage from './HeroImage';
 
 //Hook
 import { useHomeFetch } from '../hooks/userHomeFetch';
@@ -14,12 +15,24 @@ import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
 
-    const {state, loading, error} = useHomeFetch();
+    const {state  = { results: [] }, loading, error} = useHomeFetch();
 
     console.log(state);
 
 
-    return <div>Home Page</div>
+    return (
+        <>
+        {state.results[0] ? (
+        <HeroImage 
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+        title={state.results[0].original_title}
+        text={state.results[0].overview}
+        /> 
+        ) : null
+        }
+        </>
+    
+    )
 }
 
 export default Home;

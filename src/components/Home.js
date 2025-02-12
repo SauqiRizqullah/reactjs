6175b4d5c38b2252ae71f7ed8538900253cf5b78
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2025-02-12 10:10:02 +0700

    [ADD] Fetch API Data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+//API
+import API from '../API';
+
 //Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMGAE_BASE_URL } from '../config'
 //Components
@@ -13,6 +16,31 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
 
     const [error, setError] = useState(false);
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies= await API.fetchMovies(searchTerm, page);
+            // console.log(movies);
+
+            setState(prev => ({
+                ...movies, 
+                results:
+                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results] 
+        }));
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    }
+
+    // Initial Render
+    useEffect(() => {
+        fetchMovies(1)
+    }, [])
+
+    console.log(state);
 
 
     return <div>Home Page</div>
